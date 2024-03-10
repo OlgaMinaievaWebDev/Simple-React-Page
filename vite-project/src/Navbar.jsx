40e28bb915d2380144a84938f42dd7d23c0a8364
Olga <minaeva9@gmail.com>
2024-03-10 14:51:35 -0400

    nodified

---
+++ vite-project/src/Navbar.jsx
@@ -0,0 +1,12 @@
+import image from "./assets/download.png";
+
+function Navbar() {
+  return (
+    <nav>
+      <img src={image} alt="logo"></img>
+      <h3>React Facts</h3>
+      <h4>React Course - Project 1</h4>
+    </nav>
+  );
+}
+export default Navbar;
